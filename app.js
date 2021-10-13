94544b49654a636f2d4b9aaa2e4155e386aaa2e7
kiara296 <kiaraheinrich@hotmail.com>
2021-10-12 22:24:03 -0300

    corregi path imagenes en index.html y agregue el get al index.html

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
