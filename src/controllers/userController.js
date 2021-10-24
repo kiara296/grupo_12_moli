6edc48e5a0ca6d0809c19c97daa83013ee3cde45
facu sacchi <faq.ndo.n.s@gmail.com>
2021-10-24 14:32:00 -0300

    arreglo problema con los css de las views

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 const userController = {
-    login: (req, res) => { 
-        console.log ('va ok login')
-        res.render(path.join(__dirname, '../views/login.ejs')) },
-    regmoli: (req, res) => { res.render(path.join(__dirname, '../views/regmoli.ejs')) }
+  login: (req, res) => {
+    res.render(path.join(__dirname, "../views/login.ejs"));
+  },
+  regmoli: (req, res) => {
+    res.render(path.join(__dirname, "../views/regmoli.ejs"));
+  },
 };
 
-
-module.exports = userController
+module.exports = userController;
