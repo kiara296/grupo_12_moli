3a311267d0cd24d47463406114145a6316360ace
kiara296 <kiaraheinrich@hotmail.com>
2021-10-28 22:51:13 -0300

    agregue las rutas: crear productos, editar// detalle agregue /:id

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,22 @@
+// ************ Require's ************
 const express = require('express');
 const router = express.Router();
 
+// ************ Controller Require ************
 const productController = require('../controllers/productController');
 
+/* Carrito producto */
 router.get('/carrito', productController.carrito);
 
-router.get('/detail', productController.detail);
+/* Detalle producto */
+router.get('/detail/:id', productController.detail);
+
+/* Crear un producto */
+router.get('/crear', productController.crear); 
+router.post('/', productController.guardar); 
+
+/* Editar un producto */ 
+router.get('/:id/edit', productController.editar); 
+router.put('/:id', productController.update);               
 
 module.exports = router
