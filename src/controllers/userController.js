47768afee29bc720228fa8308c46d19c77e1beff
kiara296 <kiaraheinrich@hotmail.com>
2021-11-02 18:58:47 -0300

    corregi el path de res.render de login y regmoli

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,10 @@ const path = require("path");
 
 const userController = {
   login: (req, res) => {
-    res.render(path.join(__dirname, "../views/login.ejs"));
+    res.render("login");
   },
   regmoli: (req, res) => {
-    res.render(path.join(__dirname, "../views/regmoli.ejs"));
+    res.render("regmoli");
   },
 };
 
