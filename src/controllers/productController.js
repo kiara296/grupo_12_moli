f3de4239463a04b670bab72a1f2e9185d9ad849c
facu sacchi <faq.ndo.n.s@gmail.com>
2021-11-05 21:36:38 -0300

    eliminamos los flags del edit en el controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -62,9 +62,6 @@ const productController = {
       res.render("editarProductoForm", {
         productToEdit,
         category: productsService.getCategoryOptions(),
-        isRecommended,
-        isOnSale,
-        isAll,
       });
     }
   },
