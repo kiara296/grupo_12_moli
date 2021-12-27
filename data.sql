599042bba449b5ae58f063bdac533ca317d948a6
kiara296 <kiaraheinrich@hotmail.com>
2021-12-27 18:41:48 -0300

    completamos el archivo data.sql. productos + usuarios

--- data.sql
+++ data.sql
@@ -22,7 +22,21 @@ insert into product() values(
 	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
 	1
 );
-        
+insert into product() values( 
+	2,
+    'Pain au chocolat x 6 uni',
+    '400',
+    '12',
+    'Pan de chocolate 100% artesanal x 6 unidades',
+    'painChoco.png',
+    'imagen pain au chocolat',
+    'Harina de trigo  41,9%harina de trigo (gluten), gluten de trigo], agua , mantequilla  (leche) 16%, chocolate 11% , azúcar  3,8%, levadura , huevo , sal , gluten de trigo .',
+	'Precalienta tu horno a 180 ° C. Coloque los pains au chocolat en una bandeja para hornear cubierta con papel de hornear. Horno tradicional: 20 min / Horno ventilado: 15 min.  Dejar enfriar durante 10 minutos antes de degustar.',
+    '409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+    'Harina de trigo  41,9%harina de trigo (gluten), gluten de trigo], agua , mantequilla  (leche) 16%, chocolate 11% , azúcar  3,8%, levadura , huevo , sal , gluten de trigo .',
+    1
+);
+
 insert into product() values(
 	3,
 	'Tarta de espinaca grande',
@@ -80,10 +94,197 @@ insert into product() values(
 );
 
 insert into product() values(
+	7,
+	'Zapallo x 500gr',
+    500,
+    15,
+    'Zapallo en cubos IQF. 100% natural',
+    'zapalloPlato.png',
+    'imagen de zapallo',
+    'Zapallo natural 100%',
+    'Del freezer directo a tu horno o sarten. ',
+	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+    3
+);
 
+insert into product() values(
+	8,
+	'Nuggets de pollo x 500gr',
+	500,
+	15,
+	'Nuggets de pollo. 100% natural',
+	'nuggetsPollo.png',
+	'imagen de nuggets de pollo',
+	'Carne de pollo curada, cobertura 34%aceite de girasol, agua, harina de trigo (gluten), harina de maíz, pan rallado (harina de trigo (gluten), sal, levadura), sémola de trigo duro (gluten), gluten de trigo, sal, almidón de trigo (gluten), polvos de hornear (carbonatos de sodio, difosfatos), pimienta].',
+	'Del freezer directo a tu horno o sarten.',
+	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+	1
+);
+
+insert into product() values(
+	9,
+	'Ñoquis de papa x 500gr',
+	500,
+	15,
+	'Noquis de papa x 500gr. 100% natural',
+	'nioquis.png',
+	'imagen de ñoquis',
+	'Papa cocida 34,2%sémola de trigo duro (gluten), agua, papa deshidratada *, sal], leche desnatada rehidratada',
+	'Hervir agua. Separar los ñoquis. Salar el agua a gusto. Cocinar hasta que empiezen a flotar. Sacar inmediatamente. ',
+	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+	2
+);
+insert into product() values(
+	10,
+	'Croissants x 6 unidades',
+	600,
+	15,
+	'Croissants x 6 unidades. 100% caseras y natural',
+	'croissants.png',
+	'imagen de croissants',
+	'Harina de trigo * 43,8%harina de trigo (gluten), gluten de trigo], mantequilla * 24% (leche), agua *, levadura *, azúcar *, huevo (huevo, azúcar líquido) sal *, gluten de trigo. ',
+	' Precalienta tu horno a 180 ° C. Coloque los croissants en una bandeja para hornear cubierta con papel de hornear. Horno tradicional: 20 min / Horno ventilado: 15 min.  Dejar enfriar durante 10 minutos antes de degustar.',
+	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+	3
+);
+insert into product() values(
+	11,
+	'Croissants re copados',
+	'111',
+	'111',
+	'Croissants x 6 unidades. 100% caseras y natural',
+	'croissants.png',
+	'croissants',
+	'Harina de trigo * 43,8%harina de trigo (gluten), gluten de trigo], mantequilla * 24% (leche), agua *, levadura *, azúcar *, huevo (huevo, azúcar líquido) sal *, gluten de trigo. ',
+	'Precalienta tu horno a 180 ° C. Coloque los croissants en una bandeja para hornear cubierta con papel de hornear. Horno tradicional: 20 min / Horno ventilado: 15 min.  Dejar enfriar durante 10 minutos antes de degustar.',
+	'409 Kcal/51gr Hdc/17gr prot./15gr grasas tot.',
+	'Harina de trigo * 43,8%harina de trigo (gluten), gluten de trigo], mantequilla * 24% (leche), agua *, levadura *, azúcar *, huevo (huevo, azúcar líquido) sal *, gluten de trigo. ',
+     2
+);
+insert into product() values(
+	12,
+    'Pechuga de pollo orgánica al verdeo x 500gr',
+	700,
+    25,
+    'Pechuga de pollo al verdeo x 500gr. Producto organico',
+    'pollo-al-verdeo.jpg',
+    'pollo al verdeo',
+    'Pechuga de pollo organica al horno con verduras del huerta cocidas en su propio jugo ',
+    'Descongelar naturalmente en heladera. Calentar al horno a 160 grados por 15 minutos ',
+    '550 Kcal/gr Hdc/25gr prot./75gr grasas tot.',
+    3
+);
+insert into product() values(
+	13,
+    'Milanesas de soja rellenas de espinaca x 500gr',
+    400,
+    25,
+    'Milanesas caseras de soja, rellenas de espinaca y queso',
+    'milanesas-soja.jpg',
+    'milanesa de soja rellenas',
+    'Milanesa de soja rellenas de espinacas y queso',
+    'Retirar del freezer y calentar en horno (sin descongelar) por 15 minutos a fuego medio ',
+    '100 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    1
+);
+
+insert into product() values(
+	14,
+    'Pan de carne x 1Kg',
+    400,
+    25,
+    'Pan de carne casero como el que hace la abuela',
+    'pan-de-carne.jpg',
+    'pan de carne',
+    'Pan de carne preparado con carne picada magra, pan rallado, verduduras cortadas: cebolla, morrón y zanahora.',
+    'Cocinar al horno por 30 minutos a fuego medio. Opcional cubrir con mostaza. ',
+    '250 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    2
+);
+
+insert into product() values(
+	15,
+    'Albondigas con pure de papa x 500gr',
+    700,
+    25,
+    'Albondigas con pure de papas, 100% casero',
+    'albóndigas-con-puré-de-papas.jpg',
+    'albóndigas con pure de papas',
+    'Albondigas caseras con carne de res, cebolla, condimentadas, con pure de papas',
+    'Retirar del freezer y calentar en horno (sin descongelar) por 30 minutos a fuego bajo ',
+    '100 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    3
+);
+
+insert into product() values(
+	16,
+    'Paella Madrileña x 500gr',
+    800,
+    25,
+    'Paella Madrileña con mariscos y pollo con productos de primera calidad',
+    'paella-madrileña.jpg',
+    'paella',
+    'Mariscos frescos con pollo, verdudas, arroz y azafrán español',
+    'Descongelar previamente en heladera, calentar en una cacerola teflonada a fuego lento por 15/20 minutos, si es necesario agregar agua',
+    '100 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    1
+);
+insert into product() values(
+	17,
+    'Torta de chocolate selva negra',
+    1200,
+    25,
+    'Torta de chocolate con productos de primera calidad',
+    'torta-selva-negra.jpg',
+    'torta selva negra',
+    'Torta de chocolate selva negra con crema de leche y chocolate amargo',
+    'Guardar en heladera hasta consumir, se puede frezar ',
+    '500 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    2
+);
+
+insert into product() values(
+	18,
+    'Tarta de atun',
+    500,
+    25,
+    'Tarta casera de atun con verduras salteadas ',
+    'Tarta-de-atun.jpg',
+    'tarta casera de atun',
+    'Tarta casera rellena de atun y verdudas varias ',
+    'Comer fría o caliente ',
+    '150 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    3
 );
 
 
+insert into product() values(
+	19,
+    'Cazuela de mariscos x 500gr',
+    800,
+    25,
+    ' Una combinación de marisco que incluye almejas, mejillones, camarones y vieiras; le da el sabor espectacular al caldo que se combina con el sabor distintivo y natural del azafrán',
+    'cazuela-de-mariscos.jpg',
+    'cazuela de mariscos',
+    'Mariscos frescos cocinados en su caldo con azafrán natural y verduras',
+    'Descongelar previamente en heladera, calentar en una cacerola teflonada a fuego lento por 15/20 minutos, si es necesario agregar agua',
+    '200 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    1
+);
+
+insert into product() values(
+	20,
+    'Pan dulce con frutas x 500gr',
+    800,
+    25,
+    'Pan dulce bien esponjoso, casero con muchas frutas confitadas en su interior ',
+    'pan-dulce.jpg',
+    'pan dulce con frutas',
+    'Pan dulce casero con gran variedad de frutas',
+    'Guardar en un lugar fresco y consumir dentro de los 7 días ',
+    '200 Kcal/gr Hdc/25gr prot./0 gr grasas tot.',
+    2
+);
 
 insert into user() values(
 	1,
@@ -94,6 +295,93 @@ insert into user() values(
     1,
     null
 );
+
+insert into user() values(
+	2,
+   ' emelena@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Elena',
+    'Melena',
+    2,
+    null
+);
+
+insert into user() values(
+	3,
+    'prancho@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Pancho',
+    'Rancho',
+    1,
+    null
+);
+insert into user() values(
+	4,
+    'cmalandra@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Casandra',
+    'Malandra',
+    2,
+    null
+);
+insert into user() values(
+	5,
+    'lmagnesia@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Lucrecia',
+    'Magnesia',
+    1,
+    null
+  
+);
+insert into user() values(
+	6,
+    'Mmarc@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Maria',
+    'Marc',
+    2,
+    null
+);
+insert into user() values(
+	7,
+   'Mdiaz@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Marcos',
+    'Dia',
+    1,
+    null
+);
+insert into user() values(
+	8,
+    'Menola@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+   ' Marta',
+    'Enola',
+    2,
+    null
+);
+insert into user() values(
+	9,
+    'Mmarton@mail.com',
+   ' $2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Mario',
+    'Marton',
+    1,
+    null
+  
+);
+insert into user() values(
+	10,
+    'Aperez@mail.com',
+    '$2a$10$fXJqByp1snzU1VW/F9wr0u9VrI1yRr2KSoY/hXJetyhkJcRzzi4TG',
+    'Agustin',
+    'Perez',
+    1,
+    null
+);
+
+
 
 
 
